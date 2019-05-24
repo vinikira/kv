b20a4aa108ec9f5d4b7e164757ccb2fe96ca5757
Vinícius Simões <vinicius.simoes95@gmail.com>
2019-05-24 14:19:10 -0300

    add tcp server

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,10 +6,15 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
+    port =
+      String.to_integer(System.get_env("PORT") || raise("missing $PORT environment variable"))
+
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg)
       # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
